2028bdfcb30d25e7d1ed621f53b5ca97d2b13128
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-05-19 16:15:11 -0300

    cria arquivo .html

--- exercises/4.1/JavaScript-Primeiros-passos.js
+++
@@ -1,7 +0,0 @@
-const name = "Gui";
-const idade = 33;
-const birthCity = "Santa Maria - RS";
-let birthYear = 1987;
-console.log( name, birthCity, birthYear); 
-birthYear = 2030;
-console.log(birthYear);
