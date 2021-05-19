8ab5672696447ae6d021618b354dc281dff4a04d
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-05-19 19:45:22 -0300

    atualiza arquivo .js

---
+++ exercises/4.1/scripts.js
@@ -0,0 +1,15 @@
+const name = "Gui";
+const idade = 33;
+const birthCity = "Santa Maria - RS";
+let birthYear = 1987;
+console.log( name, birthCity, birthYear); 
+birthYear = 2030;
+console.log(birthYear);
+
+let a = 1
+let b = 2
+let adicao = (a + b);
+let subtracao = (a - b);
+let multiplicacao = (a * b);
+let divisao = (a / b);
+let modulo = (a % b);
