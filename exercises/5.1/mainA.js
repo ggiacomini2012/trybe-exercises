2e2b4f23f918f6d07391c9c14322c9e16459f9a8
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-05-31 11:55:39 -0300

    iniciando a semana concluindo exercicios variádos

--- exercises/5.1/mainA.js
+++ exercises/5.1/mainA.js
@@ -1,5 +1,19 @@
 const paragraph = document.getElementById("paragraph");
-paragraph.style.color = "red";
+paragraph.style.color = "blue";
 
 paragraph.innerText = "Independance Day";
 paragraph.style.backgroundColor = "black";
+
+const paragraphTwo = document.querySelector("#second-paragraph");
+paragraph.className = "classeIgual";
+paragraphTwo.className = "classeIgual";
+
+const recuperandoPorClassName = document.getElementsByClassName("classeIgual");
+
+paragraph.style.textDecoration = "underline";
+
+let subtitleByTagName = document.getElementsByTagName("h4");
+let subtitleByTagNameTwo = document.getElementById("subtitle");
+
+subtitleByTagNameTwo.style.color = "red";
+subtitleByTagName[0].style.backgroundColor = "purple";
