64c1975867b41a5ed19bdebd025ba645138aee28
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-05-31 14:07:22 -0300

    iniciando a semana concluindo exercicios variádos

--- exercises/5.1/mainC.js
+++ exercises/5.1/mainC.js
@@ -3,8 +3,31 @@
 //         - document.getElementsByClassName()
 //         - document.getElementsByTagName()
 //  Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+let euDoFuturo = document.getElementsByTagName("p")[0];
+function one() {
+  euDoFuturo.innerText = "Empregado e ganhando e performando BEM";
+  euDoFuturo.style.color = 'black';
+}
+one();
 //  Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+let quadroVerdeEhUmaOrdem = document.getElementsByClassName("main-content")[0];
+quadroVerdeEhUmaOrdem.id = "quadraVerde";
+let quadroVerdeEhUmaOrdemId = document.getElementById("quadraVerde");
+function two() {
+  quadroVerdeEhUmaOrdemId.style.backgroundColor = 'rgb(76,164,109)';
+}
+two();
 //  Crie uma função que mude a cor do quadrado vermelho para branco.
+let corrigeQuadradoVermelho = document.querySelector('.center-content');
+function three (){
+corrigeQuadradoVermelho.style.backgroundColor = 'white';
+}
+three();
 //  Crie uma função que corrija o texto da tag <h1>.
+let corrigirTexto = document.getElementsByTagName('h1')[0];
+function four() {
+    corrigirTexto.innerText = 'Exercício 5.1 - Javascript';
+}
+four();
 //  Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
 //  Crie uma função que exiba o conteúdo de todas as tags <p> no console.
