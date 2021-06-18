f5f4486f0446aef307f8d17cd2a64c303cdef51a
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-06-18 15:56:06 -0300

    exercícios 7.2

--- exercises/7.2/scripts.js
+++ exercises/7.2/scripts.js
@@ -1,13 +1,42 @@
-function bob(fu, fuf, fufu){
-    fu = {
+console.clear();
+
+function bob(fu, fuf, fufu) {
+  fu = {
     fuf: fufu,
-    }
-    console.log(fu);
-    return fu
+  };
+  console.log(fu);
+  return fu;
+}
+
+let tuy;
+let lab;
+
+bob(tuy, lab, 2);
+
+const student1 = {
+  Html: "Muito Bom",
+  Css: "Bom",
+  JavaScript: "Ótimo",
+  SoftSkills: "Ótimo",
+};
+
+let huy = "lineOne";
+huy += `
+lineTwo`;
+
+console.log(huy);
+
+console.log(`linha1
+linha2
+lineThree`);
+
+function turnObjectToText(object) {
+  for (let index = 0; index < Object.keys(student1).length; index += 1) {
+    const hability = Object.keys(object)[index];
+    const level = Object.values(object)[index];
+    const text = `${hability}, Nível: ${level}.`;
+    console.log(text);
   }
-  
-  let tuy;
-  let lab;
-  
-  bob(tuy, lab, 2);
-  
+}
+
+turnObjectToText(student1);
