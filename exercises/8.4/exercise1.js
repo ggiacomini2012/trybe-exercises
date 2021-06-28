a1e09dd6b9083fbd5d454ab5b4a28e14607d6a5d
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-06-28 17:09:28 -0300

    faz exercício 1 do 8.4

--- exercises/8.4/exercise1.js
+++ exercises/8.4/exercise1.js
@@ -1,1 +1,15 @@
-// bob
+estudantes.map((e) => ({nome: e.nome, materia: e.materias.reduce((e, a) => ((e.nota > a.nota)? e.name: a.name))}))
+
+const assert = require('assert');
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+  return arrays.reduce((acumulador, atual) => acumulador.concat(atual))
+}
+
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
