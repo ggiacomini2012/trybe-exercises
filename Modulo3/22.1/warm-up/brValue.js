2082f7d823d70cf5e4041b51797ef9231a4f084f
pro gui <ggiacomini2012@gmail.com>
2022-01-26 19:21:29 -0300

    [studying]

--- Modulo3/22.1/warm-up/brValue.js
+++
@@ -1,6 +0,0 @@
-const convert = require('./usValue');
-
-const usd = 10;
-const br = convert(usd);
-
-console.log(br);
--- Modulo3/22.1/warm-up/usValue.js
+++
@@ -1,5 +0,0 @@
-const br = 5.37;
-
-const usdToBr = (valueInUsd) => valueInUsd * br;
-
-module.exports = usdToBr;
